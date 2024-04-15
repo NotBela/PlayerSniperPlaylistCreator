068d2968af73ef0ea14bd0da799fbb6fa1ed0b05
Bela <Bela@DESTOP>
2024-04-14 21:24:22 -0400

    started work on ui, gotta fix the playerdropdownlist though because it wont work for some (no) reason

---
+++ PlayerSniperPlaylistCreator/ViewControllers/GameplaySetup/GameplaySetupViewController.cs
@@ -0,0 +1,23 @@
+﻿using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BeatSaberMarkupLanguage.Components;
+using BeatSaberMarkupLanguage.Tags;
+
+namespace PlayerSniperPlaylistCreator.ViewControllers.GameplaySetup
+{
+    [ViewDefinition("PlayerSniperPlaylistCreator.ViewControllers.GameplaySetup.GameplaySetupViewController.bsml")]
+    public class GameplaySetupViewController
+    {
+        // WHY ISNT THIS WORKING :(
+        [UIValue("playerList")]
+        private List<object> playerList = new List<object>() { "hey guys", "hi" };
+
+        
+
+    }
+}
