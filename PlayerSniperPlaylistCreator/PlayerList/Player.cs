7bae00b64cd939da0aaba34d09065e7029a736d5
Bela Varga <Bela Varga@BELASFRAMEWORK1>
2024-04-17 14:39:52 -0400

    added getting client scoresaber id from bsutils

--- PlayerSniperPlaylistCreator/PlayerList/Player.cs
+++ PlayerSniperPlaylistCreator/PlayerList/Player.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using BS_Utils;
+using BS_Utils.Gameplay;
 
 namespace PlayerSniperPlaylistCreator.PlayerList
 {
     internal class Player
     {
+        public static readonly string selfId = GetUserInfo.GetUserAsync().Result.platformUserId;
+
         public string name;
         public string id;
 
