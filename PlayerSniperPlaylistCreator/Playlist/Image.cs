b738eb94c9b04e50199624d7cd9dc98dd1f824b1
Bela <Bela@DESTOP>
2024-04-20 12:14:35 -0400

    added image class (probably not finalized yet)

---
+++ PlayerSniperPlaylistCreator/Playlist/Image.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PlayerSniperPlaylistCreator.Playlist
+{
+	internal class Image
+	{
+
+		public byte[] imageInQuestion;
+
+		public Image(byte[] imageInQuestion)
+		{
+			this.imageInQuestion = imageInQuestion;
+		}
+
+		public string convertToBase64()
+		{
+			return Convert.ToBase64String(imageInQuestion);
+		}
+	}
+}
